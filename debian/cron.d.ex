1197f0e15cb6776f96efd7ee9ab8d3c2f808bd92
jgoerzen <jpgarch@complete.org>
2003-02-12 11:08:19 -0600

    /nglister/head: changeset 11
    Removed unneeded example files
    
    darcs-hash:20030212170819-a5eff-16fffd1b21279f6844a413552165ad9d9bcdfbdb.gz

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the nglister package
-#
-0 4	* * *	root	nglister_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(nglister):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="nglister" command="/usr/bin/nglister"
